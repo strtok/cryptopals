72a0a63277d83cd97907ba42fe62d25c53928c5a
Erik Bremen <strtok@gmail.com>
2025-01-16 16:59:12 -0800

    Add 1-7

---
+++ src/set_1/challenge_7/__init__.py
@@ -0,0 +1,14 @@
+from Crypto.Cipher import AES
+from base64 import b64decode
+
+def input() -> bytes:
+    with open("data/1-7.txt") as file:
+        return b64decode("".join([line.strip() for line in file]))
+
+
+def main() -> None:
+    aes_key = b"YELLOW SUBMARINE"
+    ctext = input()
+    cipher = AES.new(aes_key, AES.MODE_ECB)
+    ptext = cipher.decrypt(ctext)
+    print(ptext)
